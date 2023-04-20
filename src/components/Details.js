4a3b721db768e7d7c06fbccd9da5979d7bc6431c
yodit <yoditabebe9391gmail.com>
2023-04-20 11:54:05 +0300

    Render details page for each device

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,40 @@
-const Details = () => (
-  <li className="list">
-    lists of devices
-  </li>
-);
+import { NavLink, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { IoChevronBackSharp } from 'react-icons/io5';
 
+const Details = () => {
+  const { devices } = useSelector((store) => store.devices);
+  const { id } = useParams();
+  return (
+    <>
+      {devices.filter((device) => device.id === id).map((device) => (
+        <div className="details" key={device.id}>
+          <h3 className="device-title">{device.name}</h3>
+          <div className="stats">Device Details</div>
+          <ul className="device-details">
+            <li className="property">
+              <span>Code</span>
+              <span>{device.code}</span>
+            </li>
+            <li className="property">
+              <span>Class</span>
+              <span>{device.class}</span>
+            </li>
+            <li className="property">
+              <span>Speciality Area</span>
+              <span>{device.speciality_area}</span>
+            </li>
+            <li className="property">
+              <span className="spec">Medical speciality description</span>
+              <span>{device.speciality_description}</span>
+            </li>
+          </ul>
+          <NavLink to="/">
+            <IoChevronBackSharp className="back-arrow" />
+          </NavLink>
+        </div>
+      ))}
+    </>
+  );
+};
 export default Details;
