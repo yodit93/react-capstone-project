77954525f7f3dcbe1126ffdd5904ea9dcc6b02c7
yodit <yoditabebe9391gmail.com>
2023-04-20 11:53:16 +0300

    Render imaging devices list inside home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,78 @@
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { IoArrowForwardCircleOutline } from 'react-icons/io5';
+import { NavLink } from 'react-router-dom';
 import { fetchDevices } from '../Redux/devicesSlices';
 
 const Home = () => {
+  const { devices, isLoading, error } = useSelector((store) => store.devices);
+  const [state, setState] = useState({
+    searchValue: '',
+    filtered: [],
+  });
+  const handleChange = (e) => {
+    const results = devices.filter((device) => {
+      if (e.target.value === '') return devices;
+      console.log(e.target.value);
+      return device.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    setState({
+      searchValue: e.target.value,
+      filtered: results,
+    });
+  };
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchDevices());
+    if (devices.length === 0) {
+      dispatch(fetchDevices());
+    }
   }, [dispatch]);
   return (
     <>
-      <div className="background-cont">
-        background image
-      </div>
-      <ul className="device-lists" />
+      <header className="header">
+        <h2 className="title">Classification of Biomedical Imaging Devices</h2>
+        <input
+          type="text"
+          className="search"
+          placeholder="Search"
+          value={state.searchValue}
+          onChange={handleChange}
+        />
+      </header>
+      <div className="stats">CLASSIFICATION BY SPECIALITY</div>
+      {isLoading && <h2>Loading...</h2>}
+      {error && <h2>{error}</h2>}
+      <ul className="device-lists">
+        {state.searchValue.length > 1 ? (
+          state.filtered.map((device) => (
+            <li className="list" key={device.id}>
+              <div className="container">
+                <h4 className="name">
+                  {device.name}
+                  <span className="sub-type">{device.submission_type}</span>
+                </h4>
+                <NavLink to={`details/${device.id}`}>
+                  <IoArrowForwardCircleOutline className="for-arrow" />
+                </NavLink>
+              </div>
+            </li>
+          ))
+        ) : (
+          devices.map((device) => (
+            <li className="list" key={device.id}>
+              <div className="container">
+                <h4 className="name">
+                  {device.name}
+                  <span className="sub-type">{device.submission_type}</span>
+                </h4>
+                <NavLink to={`details/${device.id}`}>
+                  <IoArrowForwardCircleOutline className="for-arrow" />
+                </NavLink>
+              </div>
+            </li>
+          ))
+        )}
+      </ul>
     </>
   );
 };
